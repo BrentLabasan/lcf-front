b8ce818082d042537d6cb9ecae12191853590177
Brent Labasan <BrentLabasan@gmail.com>
2018-04-03 20:49:03 -0700

    moved bootstrap theme css from node_modules to app

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,13 @@
 import 'bootstrap/dist/css/bootstrap.css';
 // import 'bootstrap/dist/css/bootstrap-theme.css';
-import 'bootstrap/dist/css/bootstrap-theme-orig.css';
+// import 'bootstrap/dist/css/bootstrap-theme-orig.css';
 
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
+import './bootstrap.css';
 
 ReactDOM.render(
   <App/>,
