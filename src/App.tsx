b00bde429e4bdff022dd778ee496dc487d87066c
Brent Labasan <BrentLabasan@gmail.com>
2018-04-03 19:19:34 -0700

    nav items

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Navbar, Grid } from 'react-bootstrap';
+import { Navbar, Nav, NavItem, NavDropdown, MenuItem, Grid } from 'react-bootstrap';
 
 import './App.css';
 
@@ -13,10 +13,26 @@ class App extends React.Component {
           <Grid>
             <Navbar.Header>
               <Navbar.Brand>
-                <a href="/">LABASAN CRYPTO FOUNTAIN<sup style={{marginLeft: 5, fontSize: 12, fontWeight: "bold", color: "orange"}}>BETA</sup></a>
+                <a href="/">LABASAN CRYPTO FOUNTAIN<sup style={{ marginLeft: 5, fontSize: 12, fontWeight: "bold", color: "orange" }}>BETA</sup></a>
               </Navbar.Brand>
               <Navbar.Toggle />
             </Navbar.Header>
+
+            <Nav>
+              <NavItem eventKey={1} href="#">
+                Link
+              </NavItem>
+              <NavItem eventKey={2} href="#">
+                Link
+              </NavItem>
+              <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
+                <MenuItem eventKey={3.1}>Action</MenuItem>
+                <MenuItem eventKey={3.2}>Another action</MenuItem>
+                <MenuItem eventKey={3.3}>Something else here</MenuItem>
+                <MenuItem divider />
+                <MenuItem eventKey={3.4}>Separated link</MenuItem>
+              </NavDropdown>
+            </Nav>
           </Grid>
         </Navbar>
       </div>
